5cb2b5dee187eacea2552447bdefc47f0399fe1d
Kateryna Nanyzniuk <65312887+katerynananyzniuk@users.noreply.github.com>
2022-10-27 12:01:17 +0300

    feat: added useState

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,19 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { AlertContext } from "../context/alert/alertContext"
 
 export const Search = () => {
+  const [value, setValue] = useState('')
   const {show} = useContext(AlertContext)
 
   const onSubmit = event => {
-    if(event.key === 'Enter') {
-      show('This is alert')
+    if (event.key !== 'Enter') {
+      return
+    }
+
+    if (value.trim()) {
+      console.log('make request with: ', value);
+    } else {
+      show('Enter user name!')
     }
   }
 
@@ -17,6 +24,8 @@ export const Search = () => {
         className="form-control"
         placeholder="Enter user name"
         onKeyPress={onSubmit}
+        value={value}
+        onChange={event => setValue(event.target.value)}
       />
     </div>
   )
